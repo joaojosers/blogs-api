d4423fb7f3d1c792f100702edac50e22765c2cc1
Joao Jose Rocha de Sousa <joaojosers@hotmail.com>
2023-10-31 20:39:20 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'display_name'
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING
+    }
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: true,
+    // createdAt: 'created_at',
+    // updatedAt: 'updated_at'
+  });
+
+  User.associate = models => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id'
+    });
+  };
+
+  return User;
+};
