fc5b663a76693455e8972ad83eddb674922d24d9
Joao Jose Rocha de Sousa <joaojosers@hotmail.com>
2023-11-08 11:15:47 -0300

    requisito 11

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const PostCategory = sequelize.define('PostCategories', {
-    PostId: {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
       type: DataTypes.INTEGER,
       foreignKey: true,
       field: 'post_id',
       allowNull: false
     },
-    CategoryId: {
+    categoryId: {
         type: DataTypes.INTEGER,
         foreignKey: true,
         field: 'category_id',
@@ -23,23 +23,35 @@ module.exports = (sequelize, DataTypes) => {
     
   });
 
-//   PostCategory.associate = models => {
-//     models.Category.belongsToMany(models.BlogPost, {
-//         as: 'blogPosts',
-//         through: PostCategory,
-//         foreignKey: 'category_id',
-//         otherKey: 'post_id',
-//     });
-//     models.BlogPost.belongsToMany(models.Category, {
-//         as: 'categories',
-//         through: PostCategory,
-//         foreignKey: 'post_id',
-//         otherKey: 'category_id',
-//     });
-//   };
-
+  PostCategory.associate = ({Category,BlogPost}) => {
+    Category.belongsToMany(BlogPost, {
+        as: 'blogPost',
+        through: PostCategory,
+        foreignKey: 'category_id',
+        otherKey: 'post_id',
+    });
+    BlogPost.belongsToMany(Category, {
+        as: 'categories',
+        through: PostCategory,
+        foreignKey: 'post_id',
+        otherKey: 'category_id',
+    });
+  }
   return PostCategory;
 };
-
-
-
+// PostCategory.associate = (models) => {
+//   models.Category.belongsToMany(models.BlogPost,
+//     {
+//       as: 'posts',
+//       through: PostCategory,
+//       foreignKey: 'categoryId',
+//       otherKey: 'postId'
+//     });
+//   models.BlogPost.belongsToMany(models.Category,
+//     {
+//       as: 'categories',
+//       through: PostCategory,
+//       foreignKey: 'postId',
+//       otherKey: 'categoryId'
+//     });
+// }
